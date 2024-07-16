b7354eb06a92339d45ca1cc771837a3fe19a84d5
Anitha Reddy Pandem <anithaeee224@gmail.com>
2024-07-16 15:45:23 +0530

    first commit

--- src/Todoapp/Todoapp.js
+++ src/Todoapp/Todoapp.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import "bootstrap/dist/css/bootstrap.css";
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
 const Todoapp = () => {
   return (
-    <div>Todoapp</div>
+    <div className="container">
+     <div className="background">
+       <h3 className="heading">Todo-Application</h3>
+       <div className="input-field">
+        <InputText style={{margin:"10px"}} />
+        <Button label="Add" style={{margin:"10px"}} />
+       </div>
+     </div>
+    </div>
   )
 }
 
